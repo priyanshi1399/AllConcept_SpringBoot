40124458a4f35d8af7ade958eb6d0a25f8025157
priyanshi1399 <priyanshitomar001@gmail.com>
2025-08-18 02:26:58 +0530

    @ProfileAnnotation

--- SpringBootProfileAnnotation/src/main/java/com/priyanshi/SpringBootProfileAnnotation/Entity/NoSqlConnection.java
+++ SpringBootProfileAnnotation/src/main/java/com/priyanshi/SpringBootProfileAnnotation/Entity/NoSqlConnection.java
@@ -1,14 +1,14 @@
 package com.priyanshi.SpringBootProfileAnnotation.Entity;
 
-
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("prod")
-public class MySQLConnection {
+@Profile("qa")
+public class NoSqlConnection {
+
 
     @Value("${username}")
     String username;
@@ -20,6 +20,5 @@ public class MySQLConnection {
     public void init(){
         System.out.println("username: "+username+ " password: "+password);
     }
-
 
 }
