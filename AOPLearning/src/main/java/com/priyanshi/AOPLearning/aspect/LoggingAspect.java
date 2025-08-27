93f5a0c7fe674544308a4113a93b103d05e8db6b
priyanshi1399 <priyanshitomar001@gmail.com>
2025-08-28 01:15:06 +0530

    Aspect Execution Poiunt cut .. and *

--- AOPLearning/src/main/java/com/priyanshi/AOPLearning/aspect/LoggingAspect.java
+++ AOPLearning/src/main/java/com/priyanshi/AOPLearning/aspect/LoggingAspect.java
@@ -8,8 +8,25 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAspect {
 
+    /*
     @Before("execution(public String com.priyanshi.AOPLearning.entity.Employee.fetchEmployee())")
     public void beforeMethod(){
         System.out.println("inside beforeMethodAspect");
     }
+    */
+
+    //wildcard
+   /* @Before("execution(* com.priyanshi.AOPLearning.entity.Employee.fetchEmployee())")
+    public void beforeMethod(){
+        System.out.println("inside beforeMethodAspect");
+    }*/
+
+    // its for methood for 0 or more item
+    @Before("execution( String com.priyanshi.AOPLearning.entity.Employee.fetchEmployee(..))")
+    public void beforeMethod(){
+        System.out.println("inside beforeMethodAspect");
+    }
+
+
+
 }
